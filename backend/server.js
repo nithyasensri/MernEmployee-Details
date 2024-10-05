fc018b62a2a15b129a037e13d3907691ef404be6
Manoj Kumar <manoj@Manojs-MacBook-Air.local>
2024-10-05 20:50:24 +0100

    Fixed bug/Added new feature

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ const bodyParser = require('body-parser');
 
 const app = express()
 app.use(express.json())
+app.use(express.static('public'));
 app.use(cors())
 app.use(bodyParser.json());// Middleware to parse URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,6 +29,9 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
+})
+app.get('/',(req,res)=>{
+    res.json("'Hello, this is the root route!'")
 })
 
 app.use('/employees', EmployeeRouter)
